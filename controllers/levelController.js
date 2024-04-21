58caeee78ba4a109d9389227da15c2ae5921aa9b
LeninHernandez <90071666+lenin123333@users.noreply.github.com>
2024-04-21 12:22:45 -0600

    modified:   controllers/levelController.js

--- controllers/levelController.js
+++ controllers/levelController.js
@@ -3,110 +3,153 @@ import { Sequelize } from "sequelize";
 
 
 
-const getLevels = async (req,res) =>{
-    
-    const {id:userId} = req.user
-    const levels = await Levels.findAll({
-        attributes: [
-          'numLevel',
-          [Sequelize.fn('MAX', Sequelize.col('numStarts')), 'maxNumStarts'],
-        ],
-        where: { userId },
-        order: [['numLevel', 'ASC']],
-        group: ['numLevel']
-      });
-      
-      if(!levels){
-        return res.json({})
-      }else{
-        return res.json(levels)
-      }
-      
-   
+const getLevels = async (req, res) => {
+
+  const { id: userId } = req.user
+  const levels = await Levels.findAll({
+    attributes: [
+      'numLevel',
+      [Sequelize.fn('MAX', Sequelize.col('numStarts')), 'maxNumStarts'],
+    ],
+    where: { userId },
+    order: [['numLevel', 'ASC']],
+    group: ['numLevel']
+  });
+
+  if (!levels) {
+    return res.json({})
+  } else {
+    return res.json(levels)
+  }
+
+
 }
 
-const addLevel = async (req,res) => {
-    const{numLevel,numStarts,nameTema,noIntentos,timeLevel} = req.body
-    
-    const {id:userId} = req.user
-   
-    
-    const existNivel= await Levels.findOne({ where: { userId,numLevel },order: [['timeLevel', 'ASC']], });
-   
-    if(!existNivel){
-        const dataLevel={
-            numLevel,
-            numStarts,
-            timeLevel,
-            userId,
-            nameTema,
-            noIntentos,
-        }
-        const level = new Levels(dataLevel)
-        level.save()
-        
-        return res.json(level)
-    }
+const addLevel = async (req, res) => {
+  const { numLevel, numStarts, nameTema, noIntentos, timeLevel } = req.body
 
-    if(existNivel.userId !== req.user.id) {
-        const error = new Error('Accion no valida')
-        return res.status(400).json({ msg: error.message })
-    }
+  const { id: userId } = req.user
 
-    const dataLevel={
-        numLevel,
-        numStarts,
-        timeLevel,
-        userId,
-        nameTema,
-        noIntentos,
+
+  const existNivel = await Levels.findOne({ where: { userId, numLevel }, order: [['timeLevel', 'ASC']], });
+
+  if (!existNivel) {
+    const dataLevel = {
+      numLevel,
+      numStarts,
+      timeLevel,
+      userId,
+      nameTema,
+      noIntentos,
     }
     const level = new Levels(dataLevel)
     level.save()
-    if(existNivel.timeLevel < timeLevel){
 
-        res.json(existNivel)
-    }else{
-        res.json(level)
-    }
-   
-    
-    
+    return res.json(level)
+  }
+
+  if (existNivel.userId !== req.user.id) {
+    const error = new Error('Accion no valida')
+    return res.status(400).json({ msg: error.message })
+  }
+
+  const dataLevel = {
+    numLevel,
+    numStarts,
+    timeLevel,
+    userId,
+    nameTema,
+    noIntentos,
+  }
+  const level = new Levels(dataLevel)
+  level.save()
+  if (existNivel.timeLevel < timeLevel) {
+
+    res.json(existNivel)
+  } else {
+    res.json(level)
+  }
+
+
+
 }
 
-const getStatistics= async (req,res) => {
-    const userId = req.params.id;
-    const existUser=await User.scope('deletePassword').findByPk(userId);
-    if(!existUser){
-        return res.status(404).json({ message: 'El Usuario No Existe' })
+const getStatistics = async (req, res) => {
+  const userId = req.params.id;
+  const existUser = await User.scope('deletePassword').findByPk(userId);
+  if (!existUser) {
+    return res.status(404).json({ message: 'El Usuario No Existe' })
+  }
+  const conteoNoIntentos = await Levels.findAll({
+    attributes: [
+      'nameTema',
+      'noIntentos',
+      [Sequelize.fn('COUNT', Sequelize.col('noIntentos')), 'total']
+    ],
+    group: ['nameTema', 'noIntentos'],
+    where: {
+      noIntentos: {
+        [Sequelize.Op.in]: [0, 1, 2, 3] // Filtrar noIntentos igual a 0, 1, 2 y 3
+      }
     }
-    const conteoNoIntentos = await Levels.findAll({
-        attributes: [
-          'nameTema',
-          'noIntentos',
-          [Sequelize.fn('COUNT', Sequelize.col('noIntentos')), 'total']
-        ],
-        group: ['nameTema', 'noIntentos'],
-        where: {
-          noIntentos: {
-            [Sequelize.Op.in]: [0, 1, 2, 3] // Filtrar noIntentos igual a 0, 1, 2 y 3
-          }
-        }
-      });
-     
-    const  count  = await Levels.aggregate('numLevel', 'DISTINCT', { plain: false });
-    return res.json({
-        conteoNoIntentos,
-        total:count.length
-    })
-      
+  });
 
-   
+  const count = await Levels.aggregate('numLevel', 'DISTINCT', { plain: false });
+  const conteoPorTema = {};
+
+  // Iterar sobre los niveles pasados
+  conteoNoIntentos.forEach(nivel => {
+    // Obtener el tema del nivel
+    const tema = nivel.nameTema;
+
+    // Verificar si ya existe el tema en el objeto de conteoPorTema
+    if (!conteoPorTema[tema]) {
+      // Si no existe, inicializar el conteo para el tema
+      conteoPorTema[tema] = {
+        correctas: 0,
+        incorrectas: 0
+      };
+    }
+
+    // Incrementar el conteo correspondiente según la respuesta
+    if (nivel.noIntentos === 0) {
+      for (let i = 0; i < nivel.dataValues.total; i++) {
+        conteoPorTema[tema].correctas++;
+      }
+
+    } else if (nivel.noIntentos === 1) {
+      for (let i = 0; i < nivel.dataValues.total; i++) {
+        conteoPorTema[tema].correctas++;
+        conteoPorTema[tema].incorrectas++;
+      }
+
+    } else if (nivel.noIntentos === 2) {
+      for (let i = 0; i < nivel.dataValues.total; i++) {
+        conteoPorTema[tema].correctas++;
+        conteoPorTema[tema].incorrectas += 2;
+      }
+
+    } else if (nivel.noIntentos === 3) {
+      for (let i = 0; i < nivel.dataValues.total; i++) {
+        conteoPorTema[tema].incorrectas += 3;
+      }
+    }
+  });
+
+  // Mostrar el resultado del conteo por tema
+  
+  return res.json({
+    conteoPorTema,
+    total: count.length
+  })
+
+
+
 
 }
 
 export {
-    addLevel,
-    getLevels,
-    getStatistics
+  addLevel,
+  getLevels,
+  getStatistics
 }
